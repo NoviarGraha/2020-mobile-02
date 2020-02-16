a43752531f2f6fbe18cb2c4c0c03bda2ed4ad585
NoviarGraha <noviaruhu@gmail.com>
2020-02-16 23:44:35 +0700

    commit done

--- app/src/main/java/id/putraprima/marketplacelayout/ProfileActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/ProfileActivity.java
@@ -1,14 +1,76 @@
 package id.putraprima.marketplacelayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView imgProfile;
+    Button imgEdit;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imgProfile = findViewById(R.id.img_profile);
+        imgEdit = findViewById(R.id.img_edit);
+
+        imgEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ){
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED){
+                        String [] permissons = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissons, PERMISSION_CODE);
+                    }else {
+                        pickImageFromGallery();
+                    }
+                }else {
+                    pickImageFromGallery();
+                }
+            }
+        });
+    }
+
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent , IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 &&grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery();
+                }else {
+                    Toast.makeText(this,"permission danied.....!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            imgProfile.setImageURI(data.getData());
+        }
     }
 }
--- app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/RegisterActivity.java
@@ -3,9 +3,12 @@ package id.putraprima.marketplacelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.opengl.EGLExt;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
     public Button btn_next;
@@ -22,10 +25,20 @@ public class RegisterActivity extends AppCompatActivity {
                 openProfilepage();
             }
         });
+
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.triang);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(RegisterActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
     }
 
     public void openProfilepage(){
         Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
+
 }
